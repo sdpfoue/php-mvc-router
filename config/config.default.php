60777ac864a58609d973e60a053067b6283f196b
Stephen <stephen.j.tong@gmail.com>
2013-05-19 16:02:49 +0800

    add base uri to config

--- config/config.default.php
+++ config/config.default.php
@@ -1,2 +1,3 @@
 <?php
 define("SITE_PATH","/path/to/site");
+define("BASE_URI", '/mvc');
--- htdocs/index.php
+++ htdocs/index.php
@@ -1,3 +1,6 @@
 <?php
+ini_set('include_path', dirname(__FILE__).'/../');
+require('../bootstrap.php');
+
 router::route();
 exit;
